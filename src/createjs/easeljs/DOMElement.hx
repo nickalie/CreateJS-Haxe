c701fd619add84bc47ca965c6e4050bc715930cd
Richard Walton <walton909@gmail.com>
2013-10-22 10:23:23 +0100

    Update DOMElement.hx
    Added missing constructor

--- src/createjs/easeljs/DOMElement.hx
+++ src/createjs/easeljs/DOMElement.hx
@@ -3,5 +3,6 @@ package createjs.easeljs;
 @:native("createjs.DOMElement")
 extern class DOMElement extends DisplayObject {
 
+	public function new(htmlElement:Dynamic):Void;
 	public var htmlElement:Dynamic;
 }
