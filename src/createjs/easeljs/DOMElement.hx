06214708f5b65bde6f5ae1dc843883f07dbfdbb9
Richard Walton <walton909@gmail.com>
2013-10-23 17:58:41 +0100

    Update DOMElement.hx
    Fixed type constraint - Was too permissive.

--- src/createjs/easeljs/DOMElement.hx
+++ src/createjs/easeljs/DOMElement.hx
@@ -1,9 +1,9 @@
 package createjs.easeljs;
 
-import js.html.HtmlElement;
+import js.html.Element;
 
 @:native("createjs.DOMElement")
-extern class DOMElement<T:HtmlElement> extends DisplayObject {
+extern class DOMElement<T:Element> extends DisplayObject {
 
 	public function new(htmlElement:T):Void;
 	public var htmlElement:T;
