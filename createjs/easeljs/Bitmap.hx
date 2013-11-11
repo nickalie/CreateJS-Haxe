8439c33fd44b78786c4393e59f2023ec0ecc8242
Richard Walton <walton909@gmail.com>
2013-11-11 14:05:36 +0000

    Update Bitmap.hx
    Added types and constructor overloads to Bitmap extern

--- createjs/easeljs/Bitmap.hx
+++ createjs/easeljs/Bitmap.hx
@@ -1,10 +1,18 @@
 package createjs.easeljs;
 
-@:native("createjs.Bitmap")
-extern class Bitmap extends DisplayObject {
+import js.html.CanvasElement;
+import js.html.ImageElement;
+import js.html.VideoElement;
 
-	public function new (imageOrUri:Dynamic):Void;
-	public var image:Dynamic;
+@:native("createjs.Bitmap")
+extern class Bitmap extends DisplayObject
+{
+	@:overload(function(url:String):Void { } )
+	@:overload(function(canvas:CanvasElement):Void { } )
+	@:overload(function(video:VideoElement):Void { } )
+	public function new(image:ImageElement):Void;
+	
+	public var image:ImageElement;
 	public var sourceRect:Rectangle;
 
 	override public function clone():Bitmap;
