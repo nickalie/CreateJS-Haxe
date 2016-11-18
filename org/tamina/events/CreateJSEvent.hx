78a2a1018e52adfceed58cbd115d36b8f1028603
David Mouton <damo@tamina-online.com>
2016-11-18 16:41:46 +0100

    add tamina utils

---
+++ org/tamina/events/CreateJSEvent.hx
@@ -0,0 +1,91 @@
+package org.tamina.events;
+
+/**
+     * CreateJSEvent Type
+     * @class CreateJSEvent
+     * @module EaselJS
+     * @static
+     */
+class CreateJSEvent {
+
+
+/**
+     * @property BITMAP_LOAD
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var BITMAP_LOAD:String='load';
+
+
+/**
+     * @property MOUSE_UP
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var MOUSE_UP:String='pressup';
+
+
+/**
+     * @property MOUSE_MOVE
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var MOUSE_MOVE:String='pressmove';
+
+
+/**
+     * @property MOUSE_DOWN
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var MOUSE_DOWN:String='mousedown';
+
+
+/**
+     * @property DOUBLE_CLICK
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var DOUBLE_CLICK:String='dblclick';
+
+
+/**
+     * @property CLICK
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var CLICK:String='click';
+
+
+/**
+     * @property TICKER_TICK
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var TICKER_TICK:String='tick';
+
+
+/**
+     * @property STAGE_MOUSE_UP
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var STAGE_MOUSE_UP:String='stagemouseup';
+
+/**
+     * @property STAGE_MOUSE_MOVE
+     * @type String
+     * @static
+     * @readOnly
+     */
+    static public inline var STAGE_MOUSE_MOVE:String='stagemousemove';
+
+}
